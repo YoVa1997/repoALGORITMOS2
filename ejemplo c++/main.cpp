1bc20e4ce71af496b5f900b25fa2373354a69fcd
carsma <carlosbass@gmail.com>
2020-10-24 10:58:53 -0600

    variable CADENA

--- ejemplo c++/main.cpp
+++ ejemplo c++/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
     cout << "Hello world 2!" << endl;
+
+    string cadena;
+
+    cadena= "hola mundo";
+
+    cout << cadena << endl;
+
     return 0;
 }
